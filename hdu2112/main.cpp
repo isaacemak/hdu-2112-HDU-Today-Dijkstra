f1f6d84546b951cf03b20cee2da248e5111f7f99
isaac <isaacemak@gmail.com>
2016-06-30 15:11:07 +0800

    Initial Commit

---
+++ hdu2112/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hdu2112
+//
+//  Created by isaac on 16/6/30.
+//  Copyright © 2016年 isaac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
